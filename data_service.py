6fcaa2ea75a06c3c1db8c3cae031fbcc6b1f35f1
Bohdan Podoliak <BohdanPodoliak1@gmail.com>
2020-11-13 10:57:38 +0200

    copy from data to process

--- data_service.py
+++ data_service.py
@@ -1,4 +1,4 @@
-"""модуль призначено для роботи з файлами вхдних даних  
+"""модуль призначено для роботи з файлами вхідних даних  
 """ 
 
 def get_Goods():
@@ -7,7 +7,7 @@ def get_Goods():
     Returns:
       Goods_list  : список товарів
     """
-    with open ("C:/Users/Formula/Documents/ICS-37630/Data/Goods.txt") as Goods_file:
+    with open ("./Data/Goods.txt") as Goods_file:
         from_file = Goods_file.readlines()
 
 
@@ -37,9 +37,59 @@ def show_Goods(Goods):
         if Goods_code_from <= Good[0] <= Goods_code_to:
             print("Код: {:8} Найменування: {:20} Торгова знижка: {:5}".format(Good[0], Good[1], Good[2]))
             count_lines +=1
+        
 
     if count_lines == 0:
         print("По вашому запиту нічого не знайдено")
 
 Goods = get_Goods()
 show_Goods(Goods)
+
+
+
+
+
+
+
+def get_Commodity_circulation():
+    """повертає список речей 
+
+    Returns:
+      Commodity_circulation_list  : список інформації
+    """
+    with open ("./Data/Commodity circulation.txt") as Commodity_circulation_file:
+        from_file = Commodity_circulation_file.readlines()
+
+
+    Commodity_circulation_list = []
+
+    for line in from_file:
+        line_list = line.split(';')
+        Commodity_circulation_list.append(line_list)
+
+
+    return Commodity_circulation_list
+
+
+
+def show_Commodity_circulation(Commodity_circulation):
+    """Виводить на екран список інформації 
+
+    Args:
+        Commodity_circulation(list): список інформації
+    """
+    Commodity_circulation_code_from = input("З я кого коду?")
+    Commodity_circulation_code_to = input("По який код?")
+
+    count_lines = 0
+
+    for Circulation in (Commodity_circulation):
+        if Commodity_circulation_code_from <= Circulation[0] <= Commodity_circulation_code_to:
+            print("Код: {:10} План: {:10} Очікуєме виконання: {:10} Рік: {:10}" .format(Circulation[0], Circulation[1], Circulation[2], Circulation[3]))
+            count_lines +=1
+
+    if count_lines == 0:
+        print("По вашому запиту нічого не знайдено")
+
+Commodity_circulation = get_Commodity_circulation ()
+show_Commodity_circulation (Commodity_circulation)
