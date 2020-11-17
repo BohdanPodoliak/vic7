f5140f9db9f34bede5a6880ab2552fb39731f019
Bohdan Podoliak <BohdanPodoliak1@gmail.com>
2020-11-17 19:31:34 +0200

    work with process_data

---
+++ process_data.py
@@ -0,0 +1,17 @@
+    """Gross income of the department store
+    """
+
+
+
+    # накопичувач рядків на валовий дохід по універмагу
+
+    zajawka = {
+
+
+        'tovarna_group_name' : '',   
+        'year' : 0,
+        'commodity circulation' : 0.0,
+        'sale' : 0.0,
+        'income' : 0.0
+
+    }
