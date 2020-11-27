562dc98e4422dc12ad54e9b896337bcddb8c366e
Bohdan Podoliak <BohdanPodoliak1@gmail.com>
2020-11-27 12:40:49 +0200

    Work with process_data

--- process_data.py
+++ process_data.py
@@ -1,17 +1,58 @@
-    """Gross income of the department store
+"""Валовий дохід універмагу
+"""
+
+from data_service import get_Goods, get_Commodity_circulation
+
+# структура рядка розрахункової таблиці
+Income = {
+    
+    'tovarna_group_name'        : '',    # назва устаткування
+    'year'                      : 0,     # назва клієнта
+    'plan circulation'          : 0.0,   # номер заказа
+    'expected_perf_circulation' : 0,     # кількість
+    'sale'                      : 0.0,   # ціна
+    'plan income'               : 0.0,   # сума
+    'expected_perf_income'      : 0.0    # Очікуване вик. (Валовий дохід) 
+}
+
+def create_Income_list():
+    """form a list
     """
+    
+    def get_tovarna_group_name(tovarna_group_code):
+        """повертає назву групи по його коді
 
 
+        Args:
+            tovarna_group_code ([type]): код код товарної групи
+        """
+        
+        for Good in Goods:
+            if tovarna_group_code == Good[0]:
+                return Good[1]
+        
+        return 'назва не знайдена'
+    
+    Income_list = []
+    
+    Goods = get_Goods()
+    Commodity_circulation = get_Commodity_circulation()
+    
+    # послідовна обробка рядків масиву 'Commodity_cirulation`
+    for Commodity in Commodity_circulation:
+        
+        # зробити робочий словник з шаблону
+        Income_work = Income.copy()
+        
+        # заповнити робочий словник значеннями 
+        Income_work['year']                          = Commodity[3]
+        Income_work['plan_circulaton']               = Commodity[1]
+        Income_work['expected_perf_circulation']     = Commodity[2]
+        Income_work['tovarna_group_name']            = get_tovarna_group_name(Commodity[0])
+        
+        # накопичити сформований рядок
+        Income_list.append(Income_work)
 
-    # накопичувач рядків на валовий дохід по універмагу
+           
+    return Income_list    
 
-    zajawka = {
-
-
-        'tovarna_group_name' : '',   
-        'year' : 0,
-        'commodity circulation' : 0.0,
-        'sale' : 0.0,
-        'income' : 0.0
-
-    }
